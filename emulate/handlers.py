e8c1a531c3e2f0f3089a9245242ca525de00b5be
Vitaly Lashmanov <lashmanov.vitaly@gmail.com>
2017-05-08 23:44:11 +0300

    Добавил proxy для перенаправления запросов по схеме маршрутизации

--- emulate/handlers.py
+++ emulate/handlers.py
@@ -7,6 +7,8 @@ from tornado.escape import json_decode
 from tornado.log import app_log
 from tornado.web import RequestHandler
 
+from emulate.mapping import RedirectMapper
+
 
 class StatusHandler(RequestHandler):
     """Обработчик получения статуса устройства."""
@@ -39,3 +41,20 @@ class StatusHandler(RequestHandler):
         }))
 
         app_log.info('Response to request {}'.format(self.args['request_id']))
+
+
+class ProxyStatusHandler(RequestHandler):
+    """Обработчик запроса на proxy-сервер."""
+
+    def post(self):
+        conf = settings.as_dict()
+        device_id = json_decode(self.request.body)['device_id']
+
+        mapper = RedirectMapper(conf['MAP'])
+        host, port = mapper.get_redirect_address(
+            device_id, tuple(conf['DEFAULT_SERVER'].values()))
+
+        redirect_url = 'http://{host}:{port}{uri}'.format(
+            host=host, port=port, uri=self.request.uri)
+
+        self.redirect(url=redirect_url, permanent=True)
---
+++ proxy.py
@@ -0,0 +1,44 @@
+import argparse
+
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.log import app_log, enable_pretty_logging
+from tornado.options import options
+from tornado.web import Application
+
+from emulate.handlers import ProxyStatusHandler
+
+
+arg_parser = argparse.ArgumentParser(
+    prog='proxy', description='Конфигурация запуска proxy-сервера')
+
+arg_parser.add_argument(
+    '--settings', '-s', type=open,  help='Маппинг серверов')
+arg_parser.add_argument(
+    '--host', '-ht', type=str, help='Хост, proxy-сервера')
+arg_parser.add_argument(
+    '--port', '-p', type=int,  help='Порт, proxy-сервера')
+
+
+def start():
+    """Запускает proxy-сервер для маршрутизации запросов."""
+    args = arg_parser.parse_args()
+
+    enable_pretty_logging(options)
+    app_log.info('Starting proxy...')
+
+    application = Application([
+        (r'/status/', ProxyStatusHandler)
+    ])
+
+    proxy_server = HTTPServer(application, xheaders=True)
+    proxy_server.listen(args.port, args.host)
+
+    IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    try:
+        start()
+    except KeyboardInterrupt:
+        app_log.info('Proxy is stopped.')
