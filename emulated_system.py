7dc06e6bee69a5fd0c9d0b4da2e4734b5bba5d56
Vitaly Lashmanov <lashmanov.vitaly@gmail.com>
2017-05-08 23:40:33 +0300

    Поправил ключи аргументов и переименовал модуль запуска аппаратно-программной системы

--- emulated_system.py
+++ emulated_system.py
@@ -1,6 +1,5 @@
 import argparse
 
-from simple_settings import settings
 from tornado.httpclient import AsyncHTTPClient
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
@@ -10,9 +9,8 @@ from tornado.web import Application
 
 from emulate.actions import PeriodicStatusSender
 from emulate.client import Device
+from emulate.handlers import StatusHandler
 from emulate.request import StatusRequest
-from handlers import StatusHandler
-from proxy import HTTPProxyServer
 
 
 arg_parser = argparse.ArgumentParser(
@@ -20,21 +18,20 @@ arg_parser = argparse.ArgumentParser(
     description='Конфигурация запуска эмулятора программно-аппаратной системы')
 
 arg_parser.add_argument(
-    '--settings', '-s', type=open,  help='Маппинг серверов')
+    '--settings', '-s', type=open,  help='Путь к файлу настройки')
 arg_parser.add_argument(
-    '--device_id', '-d', nargs='*', type=str, help='Идентификаторы устройств')
+    '--devices_id', '-d', nargs='*', type=str, help='Идентификаторы устройств')
 arg_parser.add_argument(
-    '--ip', '-i', nargs='*', type=str, help='IP-адреса устройств')
+    '--host', '-ht', type=str, help='Хост, направления статуса')
 arg_parser.add_argument(
-    '--port', '-p', nargs='*', type=int,  help='Порты устройств')
+    '--port', '-p', type=int,  help='Порт, направления статуса')
 arg_parser.add_argument(
-    '--server_port', '-sp', nargs='*', type=int,  help='Порты серверов')
+    '--servers_ports', '-sp', nargs='*', type=int,  help='Порты серверов')
 
 
 def start():
     """Запускает эмулятор программно-аппаратной системы."""
     args = arg_parser.parse_args()
-    server_map = settings.as_dict()
 
     enable_pretty_logging(options)
     app_log.info('Starting the emulation system...')
@@ -43,23 +40,15 @@ def start():
         (r'/status/', StatusHandler)
     ])
 
-    # proxy_server = HTTPProxyServer(application)
-
     # Запускаем серверы
-    for server_port in args.server_port:
+    for server_port in args.servers_ports:
         http_server = HTTPServer(application)
         http_server.listen(server_port)
 
     # Запускаем устройства(клиенты)
-    for index, device_id in enumerate(args.device_id):
-        # status_request = StatusRequest(
-        #     args.ip[index], args.port[index],
-        #     # server_map['DEFAULT']['SERVER'],
-        #     # server_map['DEFAULT']['PORT'],
-        #     device_id,
-        # )
+    for device_id in args.devices_id:
         status_sender = PeriodicStatusSender(
-            AsyncHTTPClient(), StatusRequest, args.ip[index], args.port[index])
+            AsyncHTTPClient(), StatusRequest, args.host, args.port)
         device = Device(device_id, status_sender)
         device.start()
 
